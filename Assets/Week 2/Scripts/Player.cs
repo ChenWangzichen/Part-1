8f5e7610f77e4c29aafc986d00e52dcb177ca971
ChenWangzichen <wzcjpgdzjx@gmail.com>
2024-01-18 10:13:56 -0500

    Task1 complete: collision

--- Assets/Week 2/Scripts/Player.cs
+++ Assets/Week 2/Scripts/Player.cs
@@ -10,7 +10,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,5 +23,10 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         rigidbody.AddForce(direction * force * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Player hit something");
     }
 }
